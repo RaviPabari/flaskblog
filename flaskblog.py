3485a2f747bc0a25d327c49855fb958600da82c8
RaviPabari <ravipabari@outlook.com>
2021-02-20 15:28:44 +0530

    registration added

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,12 @@
-#!/bin/python3
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+app.config['SECRET_KEY']='e79e27c3bb345bd8965a34eda10a853e'
+
+#generate random key using secret module
+# import secrets
+# secrets.token_hex(16) => 16 = 16 bytes
 
 posts = [
     {
@@ -25,6 +31,19 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html',title="About")
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!',category='success')
+        return redirect(url_for('home'))
+    return render_template('registration.html', title='Register',form=form)
+
+@app.route('/login')
+def login():
+    form = RegistrationForm()
+    return render_template('login.html', title='Login',form=form)
 
 if __name__=='__main__':
     app.run(debug=True)
